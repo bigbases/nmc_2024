23a595d0e2ec84a9189a8622800a6279ce6ef275
gggangmin <wjdrmf314@gmail.com>
2024-08-08 15:29:37 +0900

    update multihead embedding

--- nmc/models/fgmaxxvit_Multi.py
+++ nmc/models/fgmaxxvit_Multi.py
@@ -1,18 +1,17 @@
 from torch import Tensor
 from torch.nn import ModuleList
 from nmc.models.base import BaseModel
-from nmc.models.heads import MLPHead
+from nmc.models.heads import MLPMultiHead
 
 
 class FGMaxxVit_Multi(BaseModel):
-    def __init__(self, backbone: str = 'FGMaxxVit', num_classes: list = []):
+    def __init__(self, backbone: str = 'FGMaxxVit', num_classes: int=10):
         super().__init__(backbone, num_classes)
         
         
         self.head = ModuleList()
-        for num_class in num_classes:
-            
-            self.head.append(MLPHead(self.backbone.config.head_hidden_size,num_class))
+        for num_class in range(num_classes):
+            self.head.append(MLPMultiHead(num_features=self.backbone.config.head_hidden_size,num_embedding=256))
             
         self.apply(self._init_weights)
         
@@ -26,8 +25,8 @@ class FGMaxxVit_Multi(BaseModel):
     
 if __name__ == '__main__':
     import torch
-    model = FGMaxxVit('FGMaxxVit', 1000)
+    model = FGMaxxVit_Multi('FGMaxxVit', 10)
     model.init_pretrained_fgmaxxvit('checkpoints/pretrained/maxvit_base_tf_512.in1k_pretrained_weights.pth')
     x = torch.randn(2, 3, 512, 512)
-    y = model(x)
+    y = model(x,0)
     print(y.shape)
