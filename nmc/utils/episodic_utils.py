7f895384436a70134f6ec394681c49a766abb3b1
gggangmin <wjdrmf314@gmail.com>
2024-08-23 14:35:10 +0900

    update prototype func

--- nmc/utils/episodic_utils.py
+++ nmc/utils/episodic_utils.py
@@ -20,19 +20,22 @@ def compute_prototypes_multi_label(embeddings, labels):
     batch_size, n_classes, embedding_dim = embeddings.shape
     prototypes = []
     for c in range(num_classes):
-        class_mask = labels[:, c] > 0  # 특정 클래스 c에 속하는 샘플을 선택
-        if class_mask.sum() == 0: 
-            prototypes.append(torch.zeros(embedding_dim, device=embeddings.device)) # 없으면 0 vector
+        positive_mask = labels[:, c] > 0  # 특정 클래스 c에 속하는 샘플을 선택
+        if positive_mask.sum() == 0: 
+            prototypes.append(torch.tensor([torch.zeros(embedding_dim, device=embeddings.device),torch.zeros(embedding_dim, device=embeddings.device)])) # 없으면 0 vector
         else:
-            class_embeddings = embeddings[class_mask]
-            prototype = class_embeddings[:,c,:].mean(dim=0) # class 별 Embedding의 평균 추출 
-            prototypes.append(prototype)
+            negative_mask = labels[:, c] < 1
+            positive_embeddings = embeddings[positive_mask]
+            negative_embeddings = embeddings[negative_mask]
+            positive_prototype = positive_embeddings[:,c,:].mean(dim=0) # class 별 Embedding의 평균 추출
+            negative_prototype = negative_embeddings[:,c,:].mean(dim=0) 
+            prototypes.append(torch.tensor([positive_prototype,negative_prototype]))
     return torch.stack(prototypes) # ( n_class , embedding )
 
 def dot_product_similarity(query_embeddings, prototypes):
     """
     query_embeddings: (batch_size, num_classes, embedding_dim)
-    prototypes: (1, num_classes, embedding_dim)
+    prototypes: (num_classes, embedding_dim)
     """
     similarities = torch.matmul(query_embeddings, prototypes.transpose(1, 2))  # (15, 11, 11)
     similarities = similarities.diagonal(dim1=-2, dim2=-1)  # (15, 11) 대각 행렬만 추출하여 각 class 별 embedding의 유사도를 추출 
