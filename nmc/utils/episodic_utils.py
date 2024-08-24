b1fa9cc18a385b8ca9724d3381d2c0185fd48df3
gggangmin <wjdrmf314@gmail.com>
2024-08-24 11:13:07 +0900

    update similarity cal

--- nmc/utils/episodic_utils.py
+++ nmc/utils/episodic_utils.py
@@ -4,14 +4,10 @@ from torch.nn import functional as F
 
 def dot_similarity(embeddings):
         #embeddings = [batch,n_class,embedding]
-        embeddings = F.normalize(embeddings, p=2, dim=-1)
-        batch_size, n_class, embedding_dim = embeddings.shape
-        similarity = torch.zeros(n_class, batch_size, batch_size, device=embeddings.device)
-        
-        for c in range(n_class):
-            class_embeddings = embeddings[:, c, :]  # [batch, embedding_dim]
-            similarity[c] = torch.mm(class_embeddings, class_embeddings.t())
-        
+        batch_size, n_class, _ = embeddings.shape
+        # Calculate cosine similarity
+        similarity = F.cosine_similarity(embeddings.unsqueeze(2), embeddings.unsqueeze(1), dim=-1)
+    
         return similarity
 
 # START ---------------Query 유사도 계산을 위한 함수 ---------------------------
@@ -23,7 +19,7 @@ def compute_prototypes_multi_label(embeddings, labels):
         positive_mask = labels[:, c] > 0  # 특정 클래스 c에 속하는 샘플을 선택
         if positive_mask.sum() == 0:
             dumy = torch.zeros(embedding_dim, device=embeddings.device)
-            prototypes.append(torch.stack([dumy,dumy],dim=1)) # 없으면 0 vector
+            prototypes.append(torch.stack([dumy,dumy],dim=0)) # 없으면 0 vector
         else:
             negative_mask = labels[:, c] < 1
             positive_embeddings = embeddings[positive_mask]
@@ -31,20 +27,21 @@ def compute_prototypes_multi_label(embeddings, labels):
             positive_prototype = positive_embeddings[:,c,:].mean(dim=0) # class 별 Embedding의 평균 추출
             negative_prototype = negative_embeddings[:,c,:].mean(dim=0)
             
-            combined_prototypes = torch.stack([positive_prototype, negative_prototype], dim=1)
-   
+            combined_prototypes = torch.stack([positive_prototype, negative_prototype], dim=0)
+
             prototypes.append(combined_prototypes)
     return torch.stack(prototypes) # ( n_class , embedding )
 
 def dot_product_similarity(query_embeddings, prototypes):
     """
     query_embeddings: (batch_size, num_classes, embedding_dim)
-    prototypes: (num_classes, embedding_dim)
+    prototypes: (num_classes, pn, embedding_dim)
     """
-    similarities = torch.matmul(query_embeddings, prototypes.transpose(1, 2))  # (15, 11, 11)
-    similarities = similarities.diagonal(dim1=-2, dim2=-1)  # (15, 11) 대각 행렬만 추출하여 각 class 별 embedding의 유사도를 추출 
-    return similarities # ( batch size , n_class )
-# END ---------------Query 유사도 계산을 위한 함수 ---------------------------
+    query_embeddings = query_embeddings.unsqueeze(2)
+    prototypes = prototypes.unsqueeze(0)
     
+    similarities = F.cosine_similarity(query_embeddings, prototypes, dim=-1)
+    
+    return similarities
     
     
