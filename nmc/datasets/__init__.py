0e50ad12d2f0e86a09bc0bc679cb89a034cee6f3
gggangmin <wjdrmf314@gmail.com>
2024-10-15 00:48:04 +0900

    update init

--- nmc/datasets/__init__.py
+++ nmc/datasets/__init__.py
@@ -1,6 +1,6 @@
 from .APTOS import APTOSDataset, EpisodicAPTOSDataset
 from .ODIR import ODIRDataset
-from .NMC import NMCDataset, EpisodicNMCDataset
+from .NMC import NMCDataset, EpisodicNMCDataset, NMCSDataset
 
 
-__all__ = ['APTOSDataset', 'EpisodicAPTOSDataset', 'ODIRDataset', 'NMCDataset', 'EpisodicNMCDataset']
+__all__ = ['APTOSDataset', 'EpisodicAPTOSDataset', 'ODIRDataset', 'NMCDataset', 'EpisodicNMCDataset', 'NMCSDataset']
