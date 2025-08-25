7dfc8e0cf4740fda572f7e34f3ecbde311179f89
gggangmin <wjdrmf314@gmail.com>
2025-08-25 06:00:54 +0000

    Restore nmc_clean folder from remote origin/nmc-paper

---
+++ nmc_clean/core/datasets/__init__.py
@@ -0,0 +1,6 @@
+from .APTOS import APTOSDataset, EpisodicAPTOSDataset
+from .ODIR import ODIRDataset
+from .NMC import NMCDataset, EpisodicNMCDataset, NMCSDataset
+
+
+__all__ = ['APTOSDataset', 'EpisodicAPTOSDataset', 'ODIRDataset', 'NMCDataset', 'EpisodicNMCDataset', 'NMCSDataset']
